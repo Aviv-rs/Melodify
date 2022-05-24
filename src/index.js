35ab7235a4a7870686151be06349415d7ccc68fa
David Elbaz <102173191+DavidELBAZpro@users.noreply.github.com>
2022-05-24 20:11:33 +0300

    import updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import './assets/style/main.scss'
+import './assets/styles/main.scss'
 import { App } from './root-cmp'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import configureStore from './configure-store'
