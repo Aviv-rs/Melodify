0273d218b3bd923418dc9b16abae514296f8c9c3
Aviv <aviv80010@gmail.com>
2022-05-25 21:05:49 +0300

    refactored music player component and added scss

--- src/cmps/music-player.jsx
+++ src/cmps/music-player.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react'
 import YouTube from 'react-youtube'
+import { Play, Pause } from '../services/img.import.service'
 
 export const MusicPlayer = ({ videoId }) => {
     const opts = {
@@ -7,50 +8,41 @@ export const MusicPlayer = ({ videoId }) => {
         width: '0',
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
-            autoplay: 1,
         },
     }
-    const playRef = useRef()
-    const pauseRef = useRef()
-    const [videoTime, setVideoTime] = useState(null)
+
     const rangeRef = useRef()
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [videoTime, setVideoTime] = useState(null)
+    const [video, setVideo] = useState(null)
+
     const videoOnReady = ({ target }) => {
-        playRef.current.onclick = () => {
-            target.playVideo()
-        }
-        pauseRef.current.onclick = () => {
-            target.pauseVideo()
-        }
+        setVideo(target)
         setVideoTime(target.getDuration())
         rangeRef.current.onchange = (ev) => {
-            console.log('ev.target.value', ev.target.value);
             target.seekTo(+ev.target.value)
-            console.log("🚀 ~ file: music-player.jsx ~ line 29 ~ videoOnReady ~ target", target)
         }
     }
 
-    const videoOnPlay = (event) => {
-        // const player = event.target
-        // console.log('player', player.getCurrentTime())
+    const toggleVideoPlay = () => {
+        if (!video) return
+        setIsPlaying((prevIsPlaying) => !prevIsPlaying)
+        isPlaying ? video.pauseVideo() : video.playVideo()
     }
-    const onStateChange = (event) => {
-        console.log('event.data', event.data)
 
-    }
     return (
-        <div>
-            <button ref={pauseRef} >pause</button>
-            <button ref={playRef} >play</button>
-           
-
-                <input type="range" min={0} max={videoTime} ref={rangeRef} /> 
+        <div className='music-player'>
+            <div className="player-controls">
+                <button onClick={toggleVideoPlay} className="btn-toggle-play" >
+                    {isPlaying ? <Pause /> : <Play />}
+                </button>
+                <input type="range" min={0} max={videoTime} ref={rangeRef} />
                 <div> videoTime{videoTime}</div>
-          
+            </div>
+
             <YouTube videoId={videoId}
                 opts={opts}
                 onReady={videoOnReady}
-                onPlay={videoOnPlay}
-                onStateChange={onStateChange}
             />
         </div>
     )
