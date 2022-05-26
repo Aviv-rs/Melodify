0d03fd13c029b04f11e8c4b0abb9d46a5645fd01
Aviv <aviv80010@gmail.com>
2022-05-26 20:10:50 +0300

    fixed search page function names

--- src/pages/search-page.jsx
+++ src/pages/search-page.jsx
@@ -3,12 +3,12 @@ import { Search } from '../cmps/search'
 import { SongList } from '../cmps/song-list'
 export const SearchPage = () => {
     const [songs, setSongs] = useState(null)
-    const displaySongs = (songs) => {
+    const displaySongResults = (songs) => {
         setSongs(songs)
     }
     return <section className="search-page">
         <h1>Search for playlists or songs</h1>
-        <Search displaySongs={displaySongs} />
+        <Search onSearchSongs={displaySongResults} />
         <div>{songs &&
             <SongList songs={songs} isInSearchPage={true} onAddSong={null} />
         }</div>
