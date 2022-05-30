48c04046f8149b48609de0c130edaf13e224ddee
yonatan104 <yonatangeller@gmail.com>
2022-05-30 16:06:29 +0300

    play and pause bttns: station details and station preview are done song preview almost done

--- frontend/src/cmps/song-preview.jsx
+++ frontend/src/cmps/song-preview.jsx
@@ -3,6 +3,7 @@ import { setCurrSong, setIsPlaying } from '../store/actions/current-song.action'
 import { PlayIcon, PauseIcon } from '../services/img.import.service'
 import { getActionSetStation } from '../store/actions/station.action'
 import { setPlayer } from '../store/actions/player.action'
+import { useEffect, useState } from 'react'
 
 export const SongPreview = ({ song, songIdx, isSearchResult, onAddSong, station }) => {
 
@@ -10,6 +11,14 @@ export const SongPreview = ({ song, songIdx, isSearchResult, onAddSong, station 
     const { isPlaying } = useSelector(storeState => storeState.currSongModule)
     const { player } = useSelector((storeState) => storeState.playerModule)
     const { currSong } = useSelector((storeState) => storeState.currSongModule)
+    const  stationModule  = useSelector((storeState) => storeState.stationModule)
+    const [isPlayShow, setIsPlayShow] = useState(false)
+
+    useEffect(() => {
+        if(stationModule?.station?._id === station?._id 
+            && currSong?.id === song.id ) setIsPlayShow(isPlaying)
+    }, [station, isPlaying, currSong])
+
     const onTogglePlayer = () => {
         if(currSong?.id !== song.id) dispatch(setCurrSong(song))
         else if(isPlaying){
@@ -21,9 +30,11 @@ export const SongPreview = ({ song, songIdx, isSearchResult, onAddSong, station 
             dispatch(getActionSetStation(station))
         }
     }
+    
     return (<div className="song-preview" onDoubleClick={onTogglePlayer}>
         <div className='play-song-container' onClick={onTogglePlayer}>
-            <button  className="btn-play"> <PlayIcon /> </button>
+            {!isPlayShow&&<button  className="btn-play"> <PlayIcon /> </button>}
+            {isPlayShow&&<button  className="btn-play"> <PauseIcon /> </button>}
             <span className='song-number'>{songIdx +1 }</span>
         </div>
         <img src={song.imgUrl} alt="" />
