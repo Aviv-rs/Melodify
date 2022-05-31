07703ff1a19407aa32a3bba5b4c84b461298eb0e
Aviv <aviv80010@gmail.com>
2022-05-31 14:07:49 +0300

    merged duplicate headers

--- frontend/src/cmps/base/app-header.jsx
+++ frontend/src/cmps/base/app-header.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useMatch, useParams } from "react-router-dom"
 import { ArrowDownIcon } from "../../services/img.import.service"
 import { ArrowUpIcon } from "../../services/img.import.service"
 import { onLogout } from "../../store/actions/user.action"
 import { DefaultAvatarIcon } from "../../services/img.import.service"
+import { useScrollPosition } from "../../hooks/useScrollPosition"
+
 
 export const AppHeader = () => {
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const params = useParams()
+    const scrollPosition = useScrollPosition()
+
+    const { color } = useSelector(storeState => storeState.headerModule)
+    const [colorToShow, setColorToShow] = useState('')
+    const match1 = useMatch('music/station/:stationId')
+    const match2 = useMatch('music/station')
     const { user } = useSelector(storeState => storeState.userModule)
 
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    useEffect(() => {
+        if (match1 || match2) {
+            setColorToShow(color)
+        } else setColorToShow('')
+
+    }, [color, params])
 
     const onNavigate = (route) => {
         navigate(route)
@@ -26,7 +42,7 @@ export const AppHeader = () => {
         setIsMenuOpen((prevIsMenuOpen => !prevIsMenuOpen))
     }
 
-    return <header className="app-header">
+    return <header className="app-header" style={{ background: `${colorToShow}` }}>
         {user ? <button onClick={
             toggleMenuOpen}
             onBlur={toggleMenuOpen}
