b22cd20faab631fba91bfd6aa3e2ddea34f0e4c7
yonatan104 <yonatangeller@gmail.com>
2022-06-01 20:38:45 +0300

    modal tag , fixing player functionality

--- frontend/src/cmps/station/station-modal.jsx
+++ frontend/src/cmps/station/station-modal.jsx
@@ -1,29 +1,15 @@
 import { useState } from 'react'
 import { BtnExit, StationDetailsPencil, StationDefaultIcon } from '../../services/img.import.service'
-import { Select, FormControl, InputLabel, MenuItem, OutlinedInput, MenuProps } from '@mui/material'
 import { tags as allTags } from '../../data/station'
+import Multiselect from 'multiselect-react-dropdown'
 
 
 export const StationModal = ({ setIsModalOpen, handleImgUpload, station, setTitle, setDescription, onSubmit, title, description, tags, setTags }) => {
-    //TODO: add close modal option by pressing outside the modal
-    const ITEM_HEIGHT = 48;
-    const ITEM_PADDING_TOP = 8;
-    const MenuProps = {
-        PaperProps: {
-            style: {
-                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-                width: 250,
-            },
-        },
-    }
 
-    const handleChange = ({ target: { value } }) => {
-        setTags(
-            // On autofill we get a stringified value.
-            typeof value === 'string' ? value.split(',') : value,
-        )
+    const options = allTags.map(tag => { return { name: tag } })
+    const onSelect = (values) => {
+        setTags(values.map(value => value.name))
     }
-
 
     return <div className="station-modal">
         <div className='station-modal-content'>
@@ -45,55 +31,23 @@ export const StationModal = ({ setIsModalOpen, handleImgUpload, station, setTitl
                 <div className='bttns-input'>
                     <input onChange={({ target }) => setTitle(target.value)} value={title} className='title-input' type="text" placeholder='Add a name' />
                     <textarea onChange={({ target }) => setDescription(target.value)} value={description} className='album-image-description' placeholder='Add an optional description' name="" id="" cols="30" rows="10"></textarea>
-                    {/* <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                        <Select
-                            // multiple
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            // value={age}
-                            label="Add Tag"
-                            onChange={({ target }) => setTag([target])}
-                        >
-                            <MenuItem value={'Love'}>Love</MenuItem>
-                            <MenuItem value={'Pop'}>Pop</MenuItem>
-                            <MenuItem value={'House'}>House</MenuItem>
-                            <MenuItem value={'Classic'}>Classic</MenuItem>
-                            <MenuItem value={'Hip Hop'}>Hip Hop</MenuItem>
-                            <MenuItem value={'Chill'}>Chill</MenuItem>
-                            <MenuItem value={'Middle East'}>Middle East</MenuItem>
-                        </Select>
-                    </FormControl> */}
 
-
-                    <FormControl sx={{ m: 1, width: 300 }}>
-                        <InputLabel id="demo-multiple-name-label" style={{color: '#fff'}}>Name</InputLabel>
-                        <Select
-                            labelId="demo-multiple-name-label"
-                            id="demo-multiple-name"
-                            multiple
-                            value={tags}
-                            onChange={handleChange}
-                            input={<OutlinedInput label="Tag" />}
-                            MenuProps={MenuProps}
-                        >
-                            {allTags.map((tag) => (
-                                <MenuItem
-                                    key={tag}
-                                    value={tag}
-                                // style={getStyles(tag, personName, theme)}
-                                >
-                                    {tag}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
+                    <Multiselect
+                        options={options} // Options to display in the dropdown
+                        onSelect={onSelect} // Function will trigger on select event
+                        selectionLimit={3}
+                        displayValue="name" // Property name to display in the dropdown options
+                    />
+                    <button className='save-button' onClick={() => {
+                        onSubmit()
+                        setIsModalOpen(false)
+                    }
+                    }>Save</button>
                 </div>
-                <button className='save-button' onClick={() => {
-                    onSubmit()
-                    setIsModalOpen(false)
-                }}>Save</button>
+
+
             </div>
+
         </div>
 
     </div>
