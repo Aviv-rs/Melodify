d8de1fa75bc10fcc7047f76a8c95fad2a1820893
yonatan104 <yonatangeller@gmail.com>
2022-06-04 20:32:43 +0300

    adding table to like song list

--- frontend/src/cmps/liked/liked-songs-list.jsx
+++ frontend/src/cmps/liked/liked-songs-list.jsx
@@ -1,10 +1,29 @@
 import { LikedSongPreview } from './liked-song-preview'
+import { Clock } from '../../services/img.import.service'
 export const LikedSongsList = ({ songs, userStation, setLoggedUser, setSongs }) => {
 
     return (
         <div className='content-spacing'>
 
             <div className='liked-song-list'>
+
+            
+                    <div className='table-header'>
+                        <div className="song-index-container">
+                            <span className='song-index'>#</span>
+                        </div>
+                        <div className="song-title-container">
+                            <span>TITLE</span>
+                        </div>
+                        <div className="date-added-container">
+                            <span>DATE ADDED</span>
+                        </div>
+                        <div className="duration-container">
+
+                            <Clock />
+                        </div>
+                    </div>
+                
 
                 {songs.map((song, idx) => {
                     return <LikedSongPreview
