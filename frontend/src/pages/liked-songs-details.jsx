28450e6cbf360728bace8acb319ece8d15d03551
Aviv <aviv80010@gmail.com>
2022-06-04 17:22:43 +0300

    started working on liked songs hero cmp

--- frontend/src/pages/liked-songs-details.jsx
+++ frontend/src/pages/liked-songs-details.jsx
@@ -4,13 +4,11 @@ import {LikedSongList} from '../cmps/liked/liked-song-list'
 
 export const LikedSongsDetails = () => {
     const loggedUser = userService.getLoggedinUser()
-    console.log("🚀 ~ file: liked-songs.jsx ~ line 7 ~ LikedSongsDetails ~ loggedUser", loggedUser)
 
     if(loggedUser?.likedSongs)return (
-        <div>
-           <LikedSongList songs={loggedUser.likedSongs}/>
-            liked songs 
-
-        </div>
+        <section className="liked-songs-details">
+            
+           {/* <LikedSongList songs={loggedUser.likedSongs}/> */}
+        </section>
     )
 }
