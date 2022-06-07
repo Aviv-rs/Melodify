3afadf7fee771c96bbbb6c7cb01f8efc717c5fce
Aviv <aviv80010@gmail.com>
2022-06-07 18:07:38 +0300

    added calculation for template columns

--- frontend/src/cmps/station/station-list.jsx
+++ frontend/src/cmps/station/station-list.jsx
@@ -9,11 +9,24 @@ export const StationList = ({ stations }) => {
     // horizontal padding = 16 * 2 = 32
     // screenWidth
 
+
+
     const [columnCount, setColumnCount] = useState(Math.floor((window.innerWidth - 240) / 232))
 
     useEffect(() => {
         const getColumnCount = () => {
             const screenWidth = window.innerWidth
+            const minGap = 12
+            const minWidth = 180
+            // const maxWidth = 230
+
+
+        //    let columnCount = Math.floor((innerWidth - minGap) / (minWidth + minGap))
+
+        //    let columnWidth = (innerWidth - minGap) / columnCount / (1+minGap/minWidth)
+
+        //    let gap = columnWidth * minGap/minWidth
+
             setColumnCount(Math.floor((screenWidth - 240) / 232))
         }
 
@@ -25,7 +38,7 @@ export const StationList = ({ stations }) => {
     if (!stations) return
     return <section className='station-list'>
         {stations.map((station, idx) => {
-            if (station._id === 'liked' || idx >= columnCount && window.innerWidth > 640 ) return
+            if (station._id === 'liked' || idx >= columnCount && window.innerWidth > 640) return
             return <StationPreview
                 station={station}
                 key={idx}
