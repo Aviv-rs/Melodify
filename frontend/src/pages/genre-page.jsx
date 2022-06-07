531fd67ebd73122f963427b3bb0dd7a1e40ebf53
Aviv <aviv80010@gmail.com>
2022-06-07 19:50:08 +0300

    fixed genre page list

--- frontend/src/pages/genre-page.jsx
+++ frontend/src/pages/genre-page.jsx
@@ -3,27 +3,28 @@ import { useNavigate, useParams } from "react-router-dom"
 import { StationList } from "../cmps/station/station-list"
 import { stationService } from "../services/station.service"
 import { Loader } from "../cmps/util/loader"
+import { LibraryStationList } from '../cmps/station/library-station-list'
 
 export const GenrePage = () => {
 
-    const {tag} = useParams()
+    const { tag } = useParams()
     const [stations, setStations] = useState([])
     const naviagte = useNavigate()
 
-    useEffect(()=>{
-       loadStations()
-    },[])
+    useEffect(() => {
+        loadStations()
+    }, [])
 
     const loadStations = async () => {
-        const stations = await stationService.query({tags:[tag[0].toUpperCase() + tag.substring(1)]})
+        const stations = await stationService.query({ tags: [tag[0].toUpperCase() + tag.substring(1)] })
         console.log(stations)
         if (!stations) naviagte('/music/library')
         setStations(stations)
     }
-    
+
     !stations && <div className="loader-logo"><Loader /></div>
     return <section className="genre-page content-spacing">
         <h1 className="genre-title">{tag}</h1>
-        <StationList stations={stations} />
+        <LibraryStationList stations={stations} />
     </section>
 }
